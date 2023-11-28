38f8e80156281c4f90a1b8c03ab657103ebd1296
USERSITIOWEB <152224417+USERSITIOWEB@users.noreply.github.com>
2023-11-28 17:00:33 -0400

    HOLA PRIMERA VEZ

---
+++ xd/js/archivo.js
@@ -0,0 +1,7 @@
+document.getElementById('contenedor1').addEventListener('click', function() {
+    window.location.href = '../html/formulario.html';
+});
+
+document.getElementById('contenedor2').addEventListener('click', function() {
+    window.location.href = '....html';
+});
